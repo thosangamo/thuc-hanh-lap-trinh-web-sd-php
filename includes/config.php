cc52471e9acc42c4ef4cd180b86fa20f55f2d025
tuyen vu <vut41000@gmail.com>
2024-05-12 15:36:14 +0700

    config database

--- includes/config.php
+++ includes/config.php
@@ -3,12 +3,12 @@
     session_start();
     date_default_timezone_set("Asia/Kolkata");
 
-    $servername  =  "sql6.freesqldatabase.com:3306";
-    $username = "sql6705934";
-    $password = "y2dkad1zAz";
+    $servername  =  "sql107.infinityfree.com";
+    $username = "if0_36531785";
+    $password = "ZJTBeEtTMF";
 
     try {
-      $conn = new PDO("mysql:host=$servername;dbname=sql6705934", $username, $password);
+      $conn = new PDO("mysql:host=$servername;dbname=if0_36531785_youtube_database", $username, $password);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       //echo "Connected successfully";
